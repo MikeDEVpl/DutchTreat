d79ee3dd5d3fb930af9c419062d94a17e62161d8
Mike DEV <mike.dev.pl@gmail.com>
2020-07-11 14:42:59 +0200

    Models Views Annotations Validation Logging Service added

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +10,45 @@ namespace DutchTreat.Controllers
 {
     public class AppController : Controller
     {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet("contact")]  //Ręczny routing - teraz nie jest App/Contact tylko sam contact
+        public IActionResult Contact()
+        {
+          return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)    //Wszystki annotacje modelu są OK
+            {
+                //Send the email
+                _mailService.SendMessage("mojadres@ww.lk", model.Subject, $"From: {model.Name} - {model.Email}, Message:{model.Message} ");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear(); // Czyści formularz
+            }
+            else
+            {
+                //Show error
+            }
+            return View();
+
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+
             return View();
         }
     }
--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,18 +17,24 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IMailService, NullMailService>();
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if(env.IsEnvironment("Development"))
-                app.UseDeveloperExceptionPage();
+            if (env.IsEnvironment("Development"))
+            { 
+                app.UseDeveloperExceptionPage(); 
+            }
             else
-                //Add error page
-          
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
             //app.UseDefaultFiles(); // Powoduje ,¿e automatycznie wyœwietla index bez potrzeby wpisywania w pasku
+
             app.UseStaticFiles();  // Serwuje pliki z folderu wwwroot
             app.UseNodeModules();
 
